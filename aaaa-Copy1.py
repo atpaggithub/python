578091aa34438b1c7031ddb98ee688e4aaef45f5
atpaggithub <32916620+atpaggithub@users.noreply.github.com>
2020-10-15 23:07:56 -0400

    Add files via upload

---
+++ aaaa-Copy1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[94]:
+
+
+prizes={"Hannah":3,
+       "John":3,
+        "Kevin":1,
+        "Alyssa":5,
+        "Ben":4}
+
+
+for i in prizes.values():
+    if i == 4:
+        print ("A trip to Hawaii")
+    elif i == 5:
+        print ("poop")
+    else:
+        print("nothing")
+
+   
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
